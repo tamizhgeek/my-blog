7e8add8387447421c13c839b35c587de0068c06a
Azhaguselvan <azhaguselvan@gmx.com>
2009-10-12 23:41:42 +0530

    Removed a stupid/pointless plugin

--- plugins/nav_links.py
+++
@@ -1,8 +0,0 @@
-def main(blog):
-    for link in blog.settings['nav_links']:
-        for link_name,link_url in link.iteritems():
-            blog.nav_link_names.append(link_name)
-            blog.nav_link_urls.append(link_url)
-            
-
-   
