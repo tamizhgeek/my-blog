ac727925710429900a488adc19fb2f2d7a1af868
Azhagu Selvan SP <tamizhgeek@gmail.com>
2010-09-02 00:20:58 +0530

    Modified the publish script

--- publish.sh
+++ publish.sh
@@ -1,2 +1,2 @@
 git push origin master
-ssh azhagusp_azhaguselvan@ssh.phx.nearlyfreespeech.net 'cd blog && git pull origin master'
+ssh azhagusp_azhaguselvan@ssh.phx.nearlyfreespeech.net 'cd ../private/my-blog && git pull && cp -R out/ ../../public'
